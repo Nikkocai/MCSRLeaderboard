9214ceafd469856584f69e5fd7b9b6c6d68b77b2
Nikko Salvanescki <nicolas_salvanescki@hotmail.com>
2023-10-25 23:34:02 -0300

    Show categories

--- MCSRLeaderboard/Program.cs
+++ MCSRLeaderboard/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SpeedrunComSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,6 +18,18 @@ namespace MCSRLeaderboard
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            var client = new SpeedrunComClient();
+
+            var game = client.Games.SearchGame(name: "Minecraft");
+
+            MessageBox.Show("Las categorías de Minecraft que vicio me das son:");
+
+            foreach (var category in game.Categories)
+            {
+                MessageBox.Show(category.Name);
+            }
+
         }
     }
 }
